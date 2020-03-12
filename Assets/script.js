9b32d08d5b4529f405fdd1d0441760ee639f9aef
Dinatolemy <dina.a.tolemy@gmail.com>
2020-03-12 18:04:01 +1100

    final

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,5 @@
-var img1 = $('<img src="./assets/Images/Arena.png" style="width:600px; hieght=600px;">');
-var img6 = $('<img src="./assets/Images/pro.png" style="width:600px; hieght=600px;">');
+var img1 = $('<img src="./Assets/Images/Arena.png" style="width:600px; hieght=600px;">');
+var img6 = $('<img src="./Assets/Images/pro.png" style="width:600px; hieght=600px;">');
 var img3 = $('<img src="https://user-images.githubusercontent.com/55872249/70845940-8a309480-1ea8-11ea-8368-95d737dc55ce.png" alt="weather2" style="width:500px; hieght=600px;"></img>');
 var img2 = $('<img src="https://user-images.githubusercontent.com/55872249/70368243-3fd57380-18fc-11ea-91f5-2a26903189b8.png" alt="Annotation 2019-12-07 141045"style="width:500px; hieght=600px; ">');
 var img5 = $('<img src="https://user-images.githubusercontent.com/55872249/74914360-6b093280-5416-11ea-8a7f-fe7fd11a8a09.png"> alt="Eat-Da-Burger" style="width:500px; hieght=500px;')
